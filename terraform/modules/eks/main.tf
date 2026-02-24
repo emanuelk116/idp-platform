e8afc4fe798fbb0b395eed12ec4e7c341d667ccd
emanuelk116 <emanuelkovari@yahoo.com>
2026-02-24 20:16:27 +0200

    Revert "Remove private access in EKS"
    This reverts commit 4fe911cb4cf44f53b996660c53b64ef632940dd7.

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -42,6 +42,7 @@ module "eks" {
   }
 
   cluster_endpoint_public_access           = true
+  cluster_endpoint_private_access          = true
   cluster_endpoint_public_access_cidrs = ["${chomp(trimspace(data.http.my_ip.response_body))}/32"]
 
   tags = {
